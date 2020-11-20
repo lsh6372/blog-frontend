1571ca56086bd0215332ba4b4fda78c24067048b
李书红 <lishuhong@zjying1210icloudcomdeMacBook-Air.local>
2020-11-20 22:36:53 +0800

    blog

---
+++ gridsome.config.js
@@ -0,0 +1,27 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'vBlog',
+  plugins: [
+    {
+      use: '@gridsome/source-strapi',
+      options: {
+        apiURL: process.env.GRIDSOME_API_URL,
+        queryLimit: 1000, // Defaults to 100
+        contentTypes: ['project', 'blog', 'followings', 'followers'], // StrapiPost
+        // typeName: 'Strapi,
+        singleTypes: ['vblog'],
+        // Possibility to login with a Strapi user,
+        // when content types are not publicly available (optional).
+        // loginData: {
+        //   identifier: '',
+        //   password: ''
+        // }
+      }
+    }
+  ],
+}
